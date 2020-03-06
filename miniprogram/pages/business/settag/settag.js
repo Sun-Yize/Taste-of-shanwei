1fdb72b6a82291975eea56d90eaf01859304496d
Charlie Sun <inifyy@163.com>
2020-03-06 12:51:46 +0800

    app1.2   2020.3.6 12:50
    提交订单时增加评价时间
    云函数user_evaluate调整，更新此版本后需自行重新提交云函数

--- miniprogram/pages/business/settag/settag.js
+++ miniprogram/pages/business/settag/settag.js
@@ -9,13 +9,30 @@ Page({
   data: {
 
     nickname: '',
-    items: [
-      { value: '麻辣香锅', checked: '' },
-      { value: '甜品饮品', checked: '' },
-      { value: '健康早餐', checked: '' },
-      { value: '快餐', checked: '' },
-      { value: '面食', checked: '' },
-      { value: '盘餐', checked: '' },
+    items: [{
+        value: '麻辣香锅',
+        checked: ''
+      },
+      {
+        value: '甜品饮品',
+        checked: ''
+      },
+      {
+        value: '健康早餐',
+        checked: ''
+      },
+      {
+        value: '快餐',
+        checked: ''
+      },
+      {
+        value: '面食',
+        checked: ''
+      },
+      {
+        value: '盘餐',
+        checked: ''
+      },
     ],
     arr: [], //标签数组：用来存储选中的值
   },
@@ -83,66 +100,20 @@ Page({
   },
 
 
-  goindex: function (options) {
+  goindex: function(options) {
     var _this = this
-      console.log(this.data.arr)
-      var id=this.data.id
- 
+    console.log(this.data.arr)
+    var id = this.data.id
+
     //上传到数据库
     db.collection('restaurant').doc(id).update({
-      data:{
-        taste:this.data.arr
+      data: {
+        taste: this.data.arr
       }
 
     })
-
-    // db.collection('restuarant')
-    //   .where({
-    //     _openid: _.eq('userInfo.openid'),
-    //   })
-    //   .get().then(res => {
-    //     //未有记录，则增加初始空白记录
-    //     if (res.data == 0) {
-    //       console.log("未有该用户openid记录,正在添加空白记录中", res.data)
-    //       db.collection("restuarant").add({
-    //         data: {
-    //           _openid: _.eq('userInfo.openid')
-    //         },
-    //         success: function (res) {
-    //           db.collection("restuarant").add({
-    //             data: {
-    //               _id: res._id,
-    //               taste: _this.data.arr,
-    //               star: []
-    //             },
-    //             success: function (res) {
-    //               console.log("添加用户空白记录成功", res)
-    //               wx.setStorageSync('user_id', res._id)
-    //             },
-    //             fail(res) {
-    //               console.log("添加用户空白记录失败", res)
-    //             }
-    //           })
-    //         }
-    //       })
-    //     }
-    //     //已有此openid记录，则查询记录
-    //     else {
-    //       wx.setStorageSync('user_id', res.data[0]._id)
-    //       db.collection('restuarant').doc(res.data[0]._id).update({
-    //         data: {
-    //           taste: this.data.arr
-    //         },
-    //         success: function (res) {
-    //           console.log('添加成功', res)
-    //         }
-    //       })
-    //     }
-      // })
     wx.navigateBack({
-      delta:1,
-      
-
+      delta: 1,
     })
 
   },
@@ -150,52 +121,43 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     console.log(options)
-    var items=this.data.items
-    var id=options.id
+    var items = this.data.items
+    var id = options.id
     this.setData({
-      id:id
+      id: id
     })
     db.collection('restaurant').doc(id).get().then(res => {
       console.log("success")
       console.log(res.data.taste)
       this.setData({
-        arr:res.data.taste
+        arr: res.data.taste
       })
-      for(var i=0;i<6;i++){
-        for(var j=0;j<6;j++){
-          if (items[i].value == res.data.taste[j]){
-            items[i].checked=true
-
+      for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 6; j++) {
+          if (items[i].value == res.data.taste[j]) {
+            items[i].checked = true
           }
         }
-        
       }
       this.setData({
-        items:items
+        items: items
       })
-
     })
-
-
-
-
-
-
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function (option) {
+  onShow: function(option) {
     // var id=option.id
     // console.log('id')
     // this.selectEd();
@@ -210,39 +172,37 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
 
 })
-
-
--- miniprogram/pages/home/eva/eva.js
+++ miniprogram/pages/home/eva/eva.js
@@ -5,26 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-    start:' 亲，来评价下【店铺名称】的用餐体验吧，可以从菜品口味、包装和送餐时间等方面描述哦~',
-     num: 4,//后端给的分数，显示的星星
-    one_1: '',//点亮的星星数
-    two_1: '',//没有点亮的星星数
-    one_2: 5,//点亮的星星数
-    two_2: 0//没有点亮的星星数
+    start: ' 亲，来评价下【店铺名称】的用餐体验吧，可以从菜品口味、包装和送餐时间等方面描述哦~',
+    num: 4, //后端给的分数，显示的星星
+    one_1: '', //点亮的星星数
+    two_1: '', //没有点亮的星星数
+    one_2: 5, //点亮的星星数
+    two_2: 0 //没有点亮的星星数
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       order_id: options.id,
       one_1: this.data.num,
       two_1: 5 - this.data.num
-  })
+    })
   },
 
-  in_xin: function (e) {
+  in_xin: function(e) {
     var in_xin = e.currentTarget.dataset.in;
     console.log(e.currentTarget.dataset.in);
     console.log(e.currentTarget);
@@ -44,75 +44,78 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-  textFocus:function(){
+  textFocus: function() {
     this.setData({
       start: ''
     })
   },
-  textInput:function(e){
-    console.log(e)
+  textInput: function(e) {
     this.setData({
-      evaluatedetail:e.detail.value
+      evaluatedetail: e.detail.value
     })
-    console.log(this.data)
   },
-  textBlur:function(){
+  textBlur: function() {
     this.setData({
       start: '亲，来评价下【店铺名称】的用餐体验吧，可以从菜品口味、包装和送餐时间等方面描述哦~'
     })
   },
-  btnSubmit:function(){
+  btnSubmit: function() {
+    var timestamp = Date.parse(new Date());
+    var date = new Date(timestamp);
+    var time = date.getFullYear() + '-' + (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-' + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + "\t" + date.getHours() + ":" + date.getMinutes()
+
     wx.cloud.callFunction({
       name: 'user_evaluate',
       data: {
         id: this.data.order_id,
         evaluatedetail: this.data.evaluatedetail,
-        evaluate: this.data.one_2
+        evaluate: this.data.one_2,
+        etime:String(time),
       },
       success: res => {
         wx.showToast({
