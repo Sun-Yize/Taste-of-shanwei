6a3a48a7c48f380632dc1ade1c29fad5daebaaae
Charlie Sun <inifyy@163.com>
2020-03-14 23:21:38 +0800

    修改了样式

--- miniprogram/pages/home/where/where.js
+++ miniprogram/pages/home/where/where.js
@@ -35,16 +35,7 @@ Page({
   },
   //事件处理函数
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
-      //更新数据
-      that.setData({
-        userInfo: userInfo
-      })
-    })
-    this.onReset();
+    this.onReset()
   },
   // 选择学校
   onSchoolChange: function (e) {
